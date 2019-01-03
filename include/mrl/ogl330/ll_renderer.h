682591dc06a6fb96514247cf3263984e86d2942e
Ricardo Antunes <32821840+RiscadoA@users.noreply.github.com>
2019-01-03 21:31:56 +0000

    Started working on OpenGL 3.3 renderer

--- include/mrl/ogl330/ll_renderer.h
+++ include/mrl/ogl330/ll_renderer.h
@@ -1,5 +1,5 @@
-#ifndef MRL_VULKAN_LL_RENDERER_H
-#define MRL_VULKAN_LL_RENDERER_H
+#ifndef MRL_OGL330_LL_RENDERER_H
+#define MRL_OGL330_LL_RENDERER_H
 
 #include <mgl/type.h>
 #include <mrl/api_utils.h>
@@ -8,7 +8,7 @@
 extern "C" {
 #endif // __cplusplus
 
-	typedef struct mrl_vkll_renderer_t mrl_vkll_renderer_t;
+	typedef struct mrl_ogl330_ll_renderer_t mrl_ogl330_ll_renderer_t;
 	typedef struct
 	{
 		void* allocator; // Allocator used in the low level renderer.
@@ -16,29 +16,29 @@ extern "C" {
 		void(*error_callback)(const mgl_chr8_t* error); // Error callback (optional, can be NULL)
 		void(*warning_callback)(const mgl_chr8_t* msg); // Warning callback (optional, can be NULL)
 		void(*verbose_callback)(const mgl_chr8_t* msg); // Verbose callback (optional, can be NULL)
-	} mrl_vkll_renderer_creation_info_t;
+	} mrl_ogl330_ll_renderer_creation_info_t;
 
 	/// <summary>
-	///		Creates a new low level vulkan renderer.
+	///		Creates a new low level OpenGL 3.3 renderer.
 	/// </summary>
 	/// <param name="info">Renderer info</param>
 	/// <returns>Renderer handle</returns>
-	mrl_vkll_renderer_t* MRL_API mrl_create_vkll_renderer(const mrl_vkll_renderer_creation_info_t* info);
+	mrl_ogl330_ll_renderer_t* MRL_API mrl_create_ogl330_ll_renderer(const mrl_ogl330_ll_renderer_creation_info_t* info);
 	
 	/// <summary>
-	///		Destroys a low level vulkan renderer.
+	///		Destroys a low level OpenGL 3.3 renderer.
 	/// </summary>
 	/// <param name="renderer">Renderer handle</param>
-	void MRL_API mrl_destroy_vkll_renderer(mrl_vkll_renderer_t* renderer);
+	void MRL_API mrl_destroy_ogl330_ll_renderer(mrl_ogl330_ll_renderer_t* renderer);
 
 	/// <summary>
 	///		Renders a frame package.
 	/// </summary>
 	/// <param name="renderer">Renderer handle</param>
 	/// <param name="frame_package">Frame package</param>
-	void MRL_API mrl_vkll_render(mrl_vkll_renderer_t* renderer, const mgl_u8_t* frame_package);
+	void MRL_API mrl_ogl330_ll_render(mrl_ogl330_ll_renderer_t* renderer, const mgl_u8_t* frame_package);
 
 #ifdef __cplusplus
 }
 #endif // __cplusplus
-#endif // MRL_VULKAN_LL_RENDERER_H
+#endif // MRL_OGL330_LL_RENDERER_H
